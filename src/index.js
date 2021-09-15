be8a958d05acf7fa1e78ff72a804c7c232606ba2
Matheus Justino <matheussjusttino@gmail.com>
2021-09-15 20:01:20 -0300

    test: testing pwa

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { registerServiceWorker } from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -11,4 +12,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+registerServiceWorker();
 
