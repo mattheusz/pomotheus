dcd4c5b3c0f86efa89bae0bec77c79608095fd5d
Matheus Justino <matheussjusttino@gmail.com>
2021-09-24 19:14:28 -0300

    test: service worker

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,18 @@
-var CACHE_NAME = 'otimigas-app';
-
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
     '/',
-    '/static/js/bundle.js',
-    '/static/js/main.chunk.js',
-    '/static/js/1.chunk.js',
-    '/static/js/0.chunk.js',
-    '/favicon.ico',
-    '/css?family=Open+Sans',
-    '/icon?family=Material+Icons'
+    '/styles/main.css',
+    '/script/main.js'
 ];
 
-window.self.addEventListener('install', function (event) {
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', function (event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
+                console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
-    );
-});
-
-window.self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-            )
     );
 });
